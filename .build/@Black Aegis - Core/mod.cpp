f024613942b83a5a567b2ed95d1ed56d1a618fb5
Amateur-God <68581288+Amateur-God@users.noreply.github.com>
2024-09-03 17:07:59 +0100

    Delete .build directory

--- .build/@Black Aegis - Core/mod.cpp
+++
@@ -1,7 +0,0 @@
-name = "Black Aegis PMC - Core";
-picture = "bapmc.paa";
-hidePicture = "true";
-hideName = "false";
-actionName = "Discord";
-action = "https://discord.gg/2CxF43kx9V";
-description = "Viking Studios Milsim Tools, Repackeged for Black Aegies PMC";
