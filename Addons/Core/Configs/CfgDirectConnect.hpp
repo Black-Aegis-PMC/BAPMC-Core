32ac2a06ac9720d0fc61a2f0fab62c52d2c21e3a
Viking Studios <mrhowe95@gmail.com>
2024-09-03 22:59:35 +0100

    corrected overwrite due to different locations

--- Addons/Core/Configs/CfgDirectConnect.hpp
+++ Addons/Core/Configs/CfgDirectConnect.hpp
@@ -7,23 +7,42 @@ Description:
 Author:
 	Arend
 ---------------------------------------------------------------------------- */
+
 class CfgMainMenuSpotlight {
-	class Server_Join {
+	class bapmc_operations_server {
 		text = "Main Server";
 		textIsQuote = 0;
 		picture = "\x\VS_C\core\img\bapmc.paa";
 		video = "";
 		action = "connectToServer [""45.92.44.67"", 2302, """"]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
-		actionText = "Connect to server";
-		condition = "true"; //Lies
+		actionText = "Connect";
+		condition = true; //Lies
 	};
-    class Viking_Welcome {
-        text = "VKN Offical Mod";
-        textIsQuote = 0;
-        picture = "\VKN_Misc\VikingLogo512.paa";
-        video = "\VKN_Misc\VKNLOGO_512x.ogv";
-        action = "disableserialization; _script = [] execVM '\VKN_Functions\Functions\fn_ORBATCreate.sqf'; _code = uiNamespace getvariable 'CUR_bis_fnc_credits'; [_code] spawn _code;";
-        actionText = "Developed By Viking PMC";
-        condition = "false";
+
+};
+
+
+// Ovewrwrites for Viking PMC buttons
+class RscDisplayMain : RscStandardDisplay{
+
+    class Spotlight {
+        class Viking_Welcome {
+            text = "VKN Offical Mod";
+            textIsQuote = 0;
+            picture = "\VKN_Misc\VikingLogo512.paa";
+            video = "\VKN_Misc\VKNLOGO_512x.ogv";
+            action = "disableserialization; _script = [] execVM '\VKN_Functions\Functions\fn_ORBATCreate.sqf'; _code = uiNamespace getvariable 'CUR_bis_fnc_credits'; [_code] spawn _code;";
+            actionText = "Developed By Viking PMC";
+            condition = "false";
+        };
+        class Server_Join {
+            text = "Quick Join Server #1";
+            textIsQuote = 0;
+            picture = "\VKN_Misc\VikingLogo512.paa";
+            video = "";
+            action = "0 = [_this, '', '', ''] execVM '\VKN_Functions\Functions\fn_joinServer.sqf';";
+            actionText = "Direct Connect to Server";
+            condition = "false";
+        };
     };
 };
