69488ca1224687d2d0b4abe45a20f86a13a937c0
Viking Studios <mrhowe95@gmail.com>
2024-09-03 22:28:27 +0100

    overwriting menu spotlights from other mods

--- Addons/Core/Configs/CfgDirectConnect.hpp
+++ Addons/Core/Configs/CfgDirectConnect.hpp
@@ -8,13 +8,23 @@ Author:
 	Arend
 ---------------------------------------------------------------------------- */
 class CfgMainMenuSpotlight {
-	class bapmc_operations_server {
+	class Server_Join {
 		text = "Main Server";
 		textIsQuote = 0;
 		picture = "\x\VS_C\core\img\bapmc.paa";
 		video = "";
 		action = "connectToServer [""45.92.44.67"", 2302, """"]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
-		actionText = "Connect";
-		condition = true; //Lies
+		actionText = "Connect to server";
+		condition = "true"; //Lies
 	};
+        class Viking_Welcome {
+            text = "VKN Offical Mod";
+            textIsQuote = 0;
+            picture = "\VKN_Misc\VikingLogo512.paa";
+            video = "\VKN_Misc\VKNLOGO_512x.ogv";
+            action = "disableserialization; _script = [] execVM '\VKN_Functions\Functions\fn_ORBATCreate.sqf'; _code = uiNamespace getvariable 'CUR_bis_fnc_credits'; [_code] spawn _code;";
+            actionText = "Developed By Viking PMC";
+            condition = "false";
+        };
+
 };
