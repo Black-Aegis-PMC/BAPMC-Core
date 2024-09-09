b82cd25c9763c1aad84eb8f878081086fe543adf
metformin1 <69883093+metformin21@users.noreply.github.com>
2024-09-09 18:34:52 +0100

    Include password in direct connect

--- Addons/Core/Configs/CfgDirectConnect.hpp
+++ Addons/Core/Configs/CfgDirectConnect.hpp
@@ -14,7 +14,7 @@ class CfgMainMenuSpotlight {
 		textIsQuote = 0;
 		picture = "\x\VS_C\core\img\bapmc.paa";
 		video = "";
-		action = "connectToServer [""45.92.44.67"", 2302, """"]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
+		action = "connectToServer [""45.92.44.67"", 2302, ""Athena""]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
 		actionText = "Connect";
 		condition = true; //Lies
 	};
