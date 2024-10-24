c022c41210538754f8c76e58c6f6f85ddf312bda
Viking Studios <mrhowe95@gmail.com>
2024-10-24 23:18:57 +0100

    forgot this

--- Addons/Core/Configs/CfgDirectConnect.hpp
+++ Addons/Core/Configs/CfgDirectConnect.hpp
@@ -5,7 +5,7 @@ Description:
 	Registers direct connection tiles to the main menu spotlight.
 
 Author:
-	Arend
+	Carmichael
 ---------------------------------------------------------------------------- */
 
 class CfgMainMenuSpotlight {
@@ -18,7 +18,15 @@ class CfgMainMenuSpotlight {
 		actionText = "Connect";
 		condition = true; //Lies
 	};
-
+	class bapmc_Dev_server {
+		text = "Dev Server";
+		textIsQuote = 0;
+		picture = "\x\VS_C\core\img\bapmc.paa";
+		video = "";
+		action = "connectToServer [""45.92.44.67"", 2402, ""Athena""]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
+		actionText = "Connect";
+		condition = true; //Lies
+	};
 };
 
 
