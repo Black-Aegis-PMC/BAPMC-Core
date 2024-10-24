e179fcc523e753fb5ad568759433b827c7e96058
Hirisu <110851104+Hirisu@users.noreply.github.com>
2024-10-24 03:49:23 +0200

    mod compatibility

--- Addons/Weapons/config.cpp
+++ Addons/Weapons/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches{
 		version = 1.0;
 		versionAr[] = {1,0,0,0};
 		versionStr = "v1.0.0";
-		requiredAddons[] = {};
+		requiredAddons[] = {"CUP_Weapons_Compatibility","UK3CB_BAF_Weapons","rhsusf_c_weapons","SMA_Weapons"};
 		units[] = {};
 		weapons[] = {"BAPMC_baf_l115a3","BAPMC_BAF_L115A3_BL","BAPMC_baf_l115a3_ghillie","BAPMC_baf_l115a3_bl_ghillie","BAPMC_baf_l118a1_covert","BAPMC_baf_l118a1_covert_bl","BAPMC_baf_l119a1","BAPMC_baf_l119a1_cqb","BAPMC_BAF_L119A1_RIS","BAPMC_BAF_L119A1_UKUGL","BAPMC_baf_l128a1","BAPMC_BAF_L129A1","BAPMC_baf_l7a2",
 		"BAPMC_arifle_acr_dmr_blk_556", "BAPMC_smg_BallisticShield_MP7", "BAPMC_sgun_m1014", "BAPMC_sgun_m1014_solidstock", "BAPMC_sgun_m1014_vfg", "BAPMC_sgun_m1014_entry", "BAPMC_sgun_m1014_entry_vfg", "BAPMC_srifle_m110_kac_black", "BAPMC_srifle_m110_black", "BAPMC_arifle_hk_m27_vfg", "BAPMC_arifle_hk_m27", "BAPMC_arifle_hk_m27_ag36","BAPMC_lmg_mg3_rail", "BAPMC_smg_mp5a5", "BAPMC_smg_MP5A5_Rail_AFG", "BAPMC_smg_MP5A5_Rail_VFG", "BAPMC_smg_MP5A5_Rail", "BAPMC_smg_mp5sd6", "BAPMC_smg_mp7", "BAPMC_smg_p90_black", "BAPMC_smg_ps90_olive",
