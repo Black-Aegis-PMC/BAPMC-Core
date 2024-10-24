2b8f01f6b5185fab390d74c4a2ed4e59b4287969
Hirisu <110851104+Hirisu@users.noreply.github.com>
2024-10-24 02:18:03 +0200

    lets not brake other things

--- Addons/Weapons/Primarys.hpp
+++ Addons/Weapons/Primarys.hpp
@@ -1,10 +1,12 @@
 class SlotInfo;
     class CowsSlot : SlotInfo;
-    class CowsSlotBase : CowsSlot
+    {
+    };
+    class CowsSlot : CowsSlotBase  : CowsSlot
         {
 	    compatibleItems[] = { "CUP_optic_1P87_RIS", "CUP_optic_1P87_RIS_desert", "CUP_optic_1P87_RIS_woodland", "CUP_optic_Aimpoint_5000", "CUP_optic_MicroT1", "CUP_optic_AC11704_Black", "optic_Yorris", "optic_Aco", "optic_Aco_smg", "CUP_optic_HoloBlack", "CUP_optic_Eotech553_Black", "optic_Holosight", "optic_Holosight_smg", "CUP_optic_ISM1400A7", "CUP_optic_CompM2_Black", "CUP_optic_CompM2_low", "CUP_optic_MARS", "CUP_optic_ZDDot", "CUP_optic_MEPRO", "CUP_optic_OKP_7_rail", "CUP_optic_TrijiconRx01_black", "CUP_optic_VortexRazor_UH1_Black", "CUP_optic_ZeissZPoint", "optic_Hamr"};
         };
-    class CowsSlotMXM : CowsSlotBase
+    class CowsSlot : CowsSlotMXM  : CowsSlotBase
         {
 	    compatibleItems[] = { "CUP_optic_1P87_RIS", "CUP_optic_1P87_RIS_desert", "CUP_optic_1P87_RIS_woodland", "CUP_optic_Aimpoint_5000", "CUP_optic_MicroT1", "CUP_optic_AC11704_Black", "optic_Yorris", "optic_Aco", "optic_Aco_smg", "CUP_optic_HoloBlack", "CUP_optic_Eotech553_Black", "optic_Holosight", "optic_Holosight_smg", "CUP_optic_ISM1400A7", "CUP_optic_CompM2_Black", "CUP_optic_CompM2_low", "CUP_optic_MARS", "CUP_optic_ZDDot", "CUP_optic_MEPRO", "CUP_optic_OKP_7_rail", "CUP_optic_TrijiconRx01_black", "CUP_optic_VortexRazor_UH1_Black", "CUP_optic_ZeissZPoint", "optic_Hamr", "CUP_optic_1P87_1P90_BLK", "CUP_optic_G33_HWS_BLK", "CUP_optic_AIMM_COMPM2_BLK", "CUP_optic_AIMM_MARS_BLK", "CUP_optic_AIMM_ZDDOT_BLK", "CUP_optic_LeupoldMk4_CQ_T", "CUP_optic_AN_PVS_4", "optic_DMS", "CUP_optic_CWS_NV", "CUP_optic_ElcanM145", "CUP_optic_Elcan", "CUP_optic_Elcan_reflex", "CUP_optic_HensoldtZO", "ACE_optic_MRCO_2D", "CUP_optic_SB_11_4x20_PM", "CUP_optic_SUSAT", "CUP_optic_ACOG_TA01B_Black", "CUP_optic_CWS", "ACE_optic_SOS_2D", "optic_NVS", "CUP_optic_ACOG_TA648_308_Black", "UK3CB_BAF_TA648", ""};
         };
@@ -43,31 +45,31 @@ class BAPMC_baf_l118a1_covert_bl: uk3cb_baf_l118a1_covert_bl
     };
 class BAPMC_baf_l119a1: uk3cb_baf_l119a1
 	{
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_baf_l119a1_cqb: uk3cb_baf_l119a1_cqb
 	{
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_BAF_L119A1_RIS: UK3CB_BAF_L119A1_RIS
 	{
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_BAF_L119A1_UKUGL: UK3CB_BAF_L119A1_UKUGL
 	{
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_baf_l128a1: uk3cb_baf_l128a1
 	{
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_BAF_L129A1: UK3CB_BAF_L129A1
 	{
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_baf_l7a2: uk3cb_baf_l7a2
 	{
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 // CUP
 class cup_arifle_acr_dmr_blk_556; 
@@ -93,86 +95,86 @@ class cup_smg_p90_black;
 class cup_smg_ps90_olive; 
 class BAPMC_arifle_acr_dmr_blk_556: cup_arifle_acr_dmr_blk_556
 	{
-    class CowsSlotMXM
+    class CowsSlot : CowsSlotMXM 
     };
 class BAPMC_smg_BallisticShield_MP7: CUP_smg_BallisticShield_MP7
     {
     };
 class BAPMC_sgun_m1014: cup_sgun_m1014
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_sgun_m1014_solidstock: cup_sgun_m1014_solidstock
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_sgun_m1014_vfg: cup_sgun_m1014_vfg
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_sgun_m1014_entry: cup_sgun_m1014_entry
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_sgun_m1014_entry_vfg: cup_sgun_m1014_entry_vfg
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_srifle_m110_kac_black: cup_srifle_m110_kac_black
     {
-    class CowsSlotMXM
+    class CowsSlot : CowsSlotMXM 
     };
 class BAPMC_srifle_m110_black: cup_srifle_m110_black
     {
-    class CowsSlotMXM
+    class CowsSlot : CowsSlotMXM 
     };
 class BAPMC_arifle_hk_m27_vfg: cup_arifle_hk_m27_vfg
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_arifle_hk_m27: cup_arifle_hk_m27
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_arifle_hk_m27_ag36: cup_arifle_hk_m27_ag36
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_lmg_mg3_rail: cup_lmg_mg3_rail
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_mp5a5: cup_smg_mp5a5
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_MP5A5_Rail_AFG: CUP_smg_MP5A5_Rail_AFG
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_MP5A5_Rail_VFG: CUP_smg_MP5A5_Rail_VFG
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_MP5A5_Rail: CUP_smg_MP5A5_Rail
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_mp5sd6: cup_smg_mp5sd6
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_mp7: cup_smg_mp7
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_p90_black: cup_smg_p90_black
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_smg_ps90_olive: cup_smg_ps90_olive
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 // rhs
 class rhs_weap_M107; 
@@ -198,7 +200,7 @@ class BAPMC_weap_m40a5: rhs_weap_m40a5
     };
 class BAPMCusf_weap_MP7A2: rhsusf_weap_MP7A2
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 // sma
 class sma_hk416gl; 
@@ -220,69 +222,69 @@ class sma_mk16_black;
 class sma_mk16_eglm_black; 
 class BAPMC_hk416gl: sma_hk416gl
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_hk416vfg: sma_hk416vfg
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_hk416afg: sma_hk416afg
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_hk416customcqbafgb: sma_hk416customcqbafgb
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_hk416glcqb_b: sma_hk416glcqb_b
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_hk416customcqbvfgb: sma_hk416customcqbvfgb
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_hk417_16in: sma_hk417_16in
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_acrblk: sma_acrblk
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_acrgl_b: sma_acrgl_b
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_minimi_mk3_762tlb: sma_minimi_mk3_762tlb
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_minimi_mk3_762tsb: sma_minimi_mk3_762tsb
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_mk18afg: sma_mk18afg
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_mk18_gl: sma_mk18_gl
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_mk17_16_black: sma_mk17_16_black
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_mk16_blackqcb: sma_mk16_blackqcb
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_mk16_black: sma_mk16_black
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
 class BAPMC_mk16_eglm_black: sma_mk16_eglm_black
     {
-    class CowsSlotBase
+    class CowsSlot : CowsSlotBase 
     };
