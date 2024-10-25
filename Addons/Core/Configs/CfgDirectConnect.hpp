4dc600ea4e16aa329d5623632c2fd87fa1b92c80
Viking Studios <mrhowe95@gmail.com>
2024-10-25 02:40:09 +0100

    replaced ip with domain for ease of use in future

--- Addons/Core/Configs/CfgDirectConnect.hpp
+++ Addons/Core/Configs/CfgDirectConnect.hpp
@@ -14,19 +14,21 @@ class CfgMainMenuSpotlight {
 		textIsQuote = 0;
 		picture = "\x\VS_C\core\img\bapmc.paa";
 		video = "";
-		action = "connectToServer [""45.92.44.67"", 2302, ""Athena""]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
+		action = "connectToServer [""bapmc.co.uk"", 2302, ""Athena""]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
 		actionText = "Connect";
 		condition = true; //Lies
 	};
-	class bapmc_Dev_server {
+	/* Unhide in dev testing
+    class bapmc_Dev_server {
 		text = "Dev Server";
 		textIsQuote = 0;
 		picture = "\x\VS_C\core\img\bapmc.paa";
 		video = "";
-		action = "connectToServer [""45.92.44.67"", 2402, ""Athena""]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
+		action = "connectToServer [""bapmc.co.uk"", 2402, ""Athena""]"; //This Goes [""IP"", Port, ""Password""], if you dont want the password visible just leave the pasword section empty.
 		actionText = "Connect";
-		condition = false; //set to true in dev
+		condition = true; //
 	};
+    */
 };
 
 
