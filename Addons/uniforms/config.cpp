8e9f0bcf63722f47508ea1f9f0e33f21a4a11a7b
AkenoTheLabRat <112284625+AkenoTheLabRat@users.noreply.github.com>
2024-11-03 14:47:32 +0100

    added config.cpp & PBOPREFIX, moved CfgVehicles & CfgWeapons to hpp folder

---
+++ Addons/uniforms/config.cpp
@@ -0,0 +1,21 @@
+class CfgPatches
+{
+	class BAPMC_Uniforms
+	{
+		// Addon identity
+		name = "Black Aegis PMC - Uniforms";
+		author = "Black Aegis";
+		authors[] = {"AkenoTheLabRat"};
+		url = "https://discord.gg/bkdGvUNFbb";
+		version = 1.0;
+		versionAr[] = {1,0,0,0};
+		versionStr = "v1.0.0";
+		units[] = {};
+		weapons[] = {"BAPMC_CombatUniform_Black_loki","BAPMC_CombatUniform_TshirtB_Green","BAPMC_CombatUniform_TshirtB_Multi","BAPMC_CombatUniform_TshirtB_Tan","BAPMC_Black_Tshirt","BAPMC_U_B_PCUGs_BLK","BAPMC_U_B_PCUGs_BLK_R","BAPMC_U_B_PCUs","BAPMC_CombatUniform_Tactical_Sweater_7","BAPMC_Black_Black_Camo","BAPMC_black_Crye_Camo","BAPMC_Multicam_Arid_Black_casual_Camo","BAPMC_Multicam_black_casual_Camo","BAPMC_OGA_grey_casual_Camo","BAPMC_OGA_OD_casual_Camo","BAPMC_OGA_tan_casual_Camo","BAPMC_Scorpion_casual_Camo","BAPMC_Black_Black_SS_Camo","BAPMC_Black_Crye_SS_Camo","BAPMC_buttonup_uniform_b_b","BAPMC_buttonup_uniform_r_b","BAPMC_buttonup_uniform_o_b","BAPMC_buttonup_uniform_t_b","BAPMC_casual_uniform_b_b","BAPMC_casual_uniform_j_b","BAPMC_casual_uniform_t_b","BAPMC_I_B_PMC_Unit_20","BAPMC_I_B_PMC_Unit_14","BAPMC_I_B_PMC_Unit_13","BAPMC_I_B_PMC_Unit_17","BAPMC_I_B_PMC_Unit_7","BAPMC_I_B_PMC_Unit_42","BAPMC_I_B_PMC_Unit_24","BAPMC_sweater_uniform_b_b","BAPMC_sweater_uniform_g_b","BAPMC_sweater_uniform_o_b","BAPMC_sweater_uniform_t_b","BAPMC_tanktop_uniform_b_b","BAPMC_tanktop_uniform_g_b","BAPMC_tanktop_uniform_o_b","BAPMC_tee_uniform_b","BAPMC_cargo_uniform_b_b","BAPMC_cargo_uniform_b_g","BAPMC_cargo_uniform_b_o","BAPMC_cargo_uniform_b_t"};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+};
+
+#include "hpp\CfgWeapons.hpp"
+#include "hpp\CfgVehicles.hpp"
---
+++ Addons/uniforms/hpp/CfgVehicles.hpp
@@ -0,0 +1,280 @@
+class Cfgvehicles
+{
+        class VKN_CombatUniform_Black_Loki;
+        class VKN_CombatUniform_TshirtB_Green;
+        class VKN_CombatUniform_TshirtB_Multi;
+        class VKN_CombatUniform_TshirtB_Tan;
+        class VKN_Black_Tshirt;
+        class TRYK_U_B_PCUGs_BLK;
+        class TRYK_U_B_PCUGs_BLK_R;
+        class TRYK_U_B_PCUs;
+        class VKN_CombatUniform_Tactical_Sweater_7;
+        class Black_Black_Camo;
+        class black_Crye_Camo;
+        class black_Crye_Camo;
+        class Multicam_Arid_Black_casual_Camo;
+        class Multicam_Arid_Black_casual_Camo;
+        class VSM_OGA_grey_casual_Camo;
+        class VSM_OGA_OD_casual_Camo;
+        class VSM_OGA_tan_casual_Camo;
+        class VSM_Scorpion_casual_Camo;
+        class Black_Black_SS_Camo;
+        class Black_Crye_SS_Camo;
+        class PMCA_buttonup_uniform_b_b;
+        class PMCA_buttonup_uniform_r_b;
+        class PMCA_buttonup_uniform_o_b;
+        class PMCA_buttonup_uniform_t_b;
+        class PMCA_casual_uniform_b_b;
+        class PMCA_casual_uniform_j_b;
+        class PMCA_casual_uniform_t_b;
+        class CUP_I_B_PMC_Unit_20;
+        class CUP_I_B_PMC_Unit_14;
+        class CUP_I_B_PMC_Unit_13;
+        class CUP_I_B_PMC_Unit_17;
+        class CUP_I_B_PMC_Unit_7;
+        class CUP_I_B_PMC_Unit_42;
+        class CUP_I_B_PMC_Unit_24;
+        class PMCA_sweater_uniform_b_b;
+        class PMCA_sweater_uniform_g_b;
+        class PMCA_sweater_uniform_o_b;
+        class PMCA_sweater_uniform_t_b;
+        class PMCA_tanktop_uniform_b_b;
+        class PMCA_tanktop_uniform_g_b;
+        class PMCA_tanktop_uniform_o_b;
+        class PMCA_tee_uniform_b;
+        class PMCA_cargo_uniform_b_b;
+        class PMCA_cargo_uniform_b_g;
+        class PMCA_cargo_uniform_b_o;
+        class PMCA_cargo_uniform_b_t;
+
+        class BAPMC_uniform_CombatUniform_Black_loki: VKN_CombatUniform_Black_Loki
+        {
+            uniformclass = "BAPMC_CombatUniform_Black_loki";
+        };
+
+        class BAPMC_uniform_CombatUniform_TshirtB_Green: VKN_CombatUniform_TshirtB_Green
+        {
+            uniformclass = "BAPMC_CombatUniform_TshirtB_Green"
+        };
+
+        class BAPMC_uniform_CombatUniform_TshirtB_Multi: VKN_CombatUniform_TshirtB_Multi
+        {
+            uniformclass = "BAPMC_CombatUniform_TshirtB_Multi"
+        };
+
+        class BAPMC_uniform_CombatUniform_TshirtB_Tan: VKN_CombatUniform_TshirtB_Tan
+        {;
+            uniformclass = "BAPMC_CombatUniform_TshirtB_Tan"
+        };
+
+       class BAPMC_uniform_Black_Tshirt: VKN_Black_Tshirt
+        {;
+            uniformclass = "BAPMC_Black_Tshirt"
+        }; 
+
+        class BAPMC_uniform_U_B_PCUGs_BLK: TRYK_U_B_PCUGs_BLK
+        {;
+            uniformclass = "BAPMC_U_B_PCUGs_BLK"
+        }; 
+
+        class BAPMC_uniform_U_B_PCUGs_BLK_R: TRYK_U_B_PCUGs_BLK_R
+        {;
+            uniformclass = "BAPMC_U_B_PCUGs_BLK_R"
+        };
+
+        class BAPMC_uniform_U_B_PCUs: TRYK_U_B_PCUs
+        {;
+            uniformclass = "BAPMC_U_B_PCUs"
+        };
+
+        class BAPMC_unifrom_CombatUniform_Tactical_Sweater_7: VKN_CombatUniform_Tactical_Sweater_7
+        {;
+            uniformclass = "BAPMC_CombatUniform_Tactical_Sweater_7"
+        };
+
+        class BAPMC_uniform_Black_Black_Camo: Black_Black_Camo
+        {;
+            uniformclass = "BAPMC_Black_Black_Camo"
+        };
+
+        class BAPMC_uniform_black_Crye_Camo: black_Crye_Camo
+        {;
+            uniformclass = "BAPMC_black_Crye_Camo"
+        };
+
+        class BAPMC_uniform_black_Crye_Camo: black_Crye_Camo
+        {;
+            uniformclass = "BAPMC_black_Crye_Camo"
+        };
+
+        class BAPMC_uniform_Multicam_Arid_Black_casual_Camo:  Multicam_Arid_Black_casual_Camo
+        {;
+            uniformclass = "BAPMC_Multicam_Arid_Black_casual_Camo"
+        };
+
+        class BAPMC_uniform_Multicam_black_casual_Camo:  Multicam_black_casual_Camo
+        {;
+            uniformclass = "BAPMC_Multicam_black_casual_Camo"
+        };
+
+        class BAPMC_uniform_OGA_grey_casual_Camo:  VSM_OGA_grey_casual_Camo
+        {;
+            uniformclass = "BAPMC_OGA_grey_casual_Camo"
+        };
+
+        class BAPMC_uniform_OGA_OD_casual_Camo:  VSM_OGA_OD_casual_Camo
+        {;
+            uniformclass = "BAPMC_OGA_OD_casual_Camo"
+        };
+
+        class BAPMC_uniform_OGA_tan_casual_Camo:  VSM_OGA_tan_casual_Camo
+        {;
+            uniformclass = "BAPMC_OGA_tan_casual_Camo"
+        };
+
+        class BAPMC_uniform_Scorpion_casual_Camo:  VSM_Scorpion_casual_Camo
+        {;
+            uniformclass = "BAPMC_Scorpion_casual_Camo"
+        };
+
+        class BAPMC_uniform_Black_Black_SS_Camo:  Black_Black_SS_Camo
+        {;
+            uniformclass = "BAPMC_Black_Black_SS_Camo"
+        };
+
+        class BAPMC_uniform_Black_Crye_SS_Camo:  Black_Crye_SS_Camo
+        {;
+            uniformclass = "BAPMC_Black_Crye_SS_Camo"
+        };
+
+        class BAPMC_uniform_buttonup_uniform_b_b:  PMCA_buttonup_uniform_b_b
+        {;
+            uniformclass = "BAPMC_buttonup_uniform_b_b"
+        };
+
+        class BAPMC_uniform_buttonup_uniform_r_b:  PMCA_buttonup_uniform_r_b
+        {;
+            uniformclass = "BAPMC_buttonup_uniform_r_b"
+        };
+
+        class BAPMC_uniform_buttonup_uniform_o_b:  PMCA_buttonup_uniform_o_b
+        {;
+            uniformclass = "BAPMC_buttonup_uniform_o_b"
+        };
+
+        class BAPMC_uniform_buttonup_uniform_t_b:  PMCA_buttonup_uniform_t_b
+        {;
+            uniformclass = "BAPMC_buttonup_uniform_t_b"
+        };
+
+        class BAPMC_uniform_casual_uniform_b_b:  PMCA_casual_uniform_b_b
+        {;
+            uniformclass = "BAPMC_casual_uniform_b_b"
+        };
+
+        class BAPMC_uniform_casual_uniform_j_b:  PMCA_casual_uniform_j_b
+        {;
+            uniformclass = "PMCA_casual_uniform_j_b"
+        };
+
+        class BAPMC_uniform_casual_uniform_t_b:  PMCA_casual_uniform_t_b
+        {;
+            uniformclass = "PMCA_casual_uniform_t_b"
+        };
+
+        class BAPMC_uniform_I_B_PMC_Unit_20:  CUP_I_B_PMC_Unit_20
+        {;
+            uniformclass = "CUP_I_B_PMC_Unit_20"
+        };
+
+        class BAPMC_uniform_I_B_PMC_Unit_14:  CUP_I_B_PMC_Unit_14
+        {;
+            uniformclass = "CUP_I_B_PMC_Unit_14"
+        };
+
+        class BAPMC_uniform_I_B_PMC_Unit_13:  CUP_I_B_PMC_Unit_13
+        {;
+            uniformclass = "CUP_I_B_PMC_Unit_13"
+        };
+
+        class BAPMC_uniform_I_B_PMC_Unit_17:  CUP_I_B_PMC_Unit_17
+        {;
+            uniformclass = "CUP_I_B_PMC_Unit_17"
+        };
+
+        class BAPMC_uniform_I_B_PMC_Unit_7:  CUP_I_B_PMC_Unit_7
+        {;
+            uniformclass = "CUP_I_B_PMC_Unit_7"
+        };
+
+        class BAPMC_uniform_I_B_PMC_Unit_42:  CUP_I_B_PMC_Unit_42
+        {;
+            uniformclass = "CUP_I_B_PMC_Unit_42"
+        };
+
+        class BAPMC_uniform_I_B_PMC_Unit_24:  CUP_I_B_PMC_Unit_24
+        {;
+            uniformclass = "CUP_I_B_PMC_Unit_24"
+        };
+
+        class BAPMC_uniform_sweater_uniform_b_b:  PMCA_sweater_uniform_b_b
+        {;
+            uniformclass = "PMCA_sweater_uniform_b_b"
+        };
+
+        class BAPMC_uniform_sweater_uniform_g_b:  PMCA_sweater_uniform_g_b
+        {;
+            uniformclass = "PMCA_sweater_uniform_g_b"
+        };
+
+        class BAPMC_uniform_sweater_uniform_o_b:  PMCA_sweater_uniform_o_b
+        {;
+            uniformclass = "PMCA_sweater_uniform_o_b"
+        };
+
+        class BAPMC_uniform_sweater_uniform_t_b:  PMCA_sweater_uniform_t_b
+        {;
+            uniformclass = "PMCA_sweater_uniform_t_b"
+        };
+
+        class BAPMC_uniform_tanktop_uniform_b_b:  PMCA_tanktop_uniform_b_b
+        {;
+            uniformclass = "PMCA_tanktop_uniform_b_b"
+        };
+
+        class BAPMC_uniform_tanktop_uniform_g_b:  PMCA_tanktop_uniform_g_b
+        {;
+            uniformclass = "PMCA_tanktop_uniform_g_b"
+        };
+
+        class BAPMC_uniform_tanktop_uniform_o_b:  PMCA_tanktop_uniform_o_b
+        {;
+            uniformclass = "PMCA_tanktop_uniform_o_b"
+        };
+
+        class BAPMC_uniform_tee_uniform_b:  PMCA_tee_uniform_b
+        {;
+            uniformclass = "PMCA_tee_uniform_b"
+        };
+
+        class BAPMC_uniform_cargo_uniform_b_b:  PMCA_cargo_uniform_b_b
+        {;
+            uniformclass = "PMCA_cargo_uniform_b_b"
+        };
+
+        class BAPMC_uniform_cargo_uniform_b_g:  PMCA_cargo_uniform_b_g
+        {;
+            uniformclass = "PMCA_cargo_uniform_b_g"
+        };
+
+        class BAPMC_uniform_cargo_uniform_b_o:  PMCA_cargo_uniform_b_o
+        {;
+            uniformclass = "PMCA_cargo_uniform_b_o"
+        };
+
+        class BAPMC_uniform_cargo_uniform_b_t:  PMCA_cargo_uniform_b_t
+        {;
+            uniformclass = "PMCA_cargo_uniform_b_t"
+        };
+
+}
