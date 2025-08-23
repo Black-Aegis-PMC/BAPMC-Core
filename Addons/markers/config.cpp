2e2b65e673bb53a23dbbe699e221e17fbb40d083
Welshy <welshyplaysgames@gmail.com>
2025-08-23 16:41:37 +0100

    Changed Logos, and PBOPREFIX for markers to ensure PboProject builds properlly. Also added an alternative to the main marker, added dragon and havoc team markers (cause why not)

--- Addons/markers/config.cpp
+++ Addons/markers/config.cpp
@@ -3,17 +3,17 @@ class CfgPatches
     class VS_Markers
     {
         name = "Black Aegis - Markers";
-		author = "Viking Studios";
-		authors[] = {"Amateur-god"};
-		url = "https://discord.gg/bkdGvUNFbb";
-		version = 1.0;
-		versionAr[] = {1,0,0,0};
-		versionStr = "v1.0.0";
-		units[] = {"VS_BA_Flag"};
-        weapons[]={};
-		magazines[] = {};
-		ammo[] = {};
-		worlds[] = {};
+        author = "Viking Studios";
+        authors[] = {"Amateur-god", "Welshy"};
+        url = "https://discord.gg/bkdGvUNFbb";
+        version = 1.0;
+        versionAr[] = {1,0,0,0};
+        versionStr = "v1.0.0";
+        units[] = {};
+        weapons[] = {};
+        magazines[] = {};
+        ammo[] = {};
+        worlds[] = {};
     };
 };
 
@@ -30,7 +30,43 @@ class CfgMarkers
     class VS_BA_Flag
     {
         name = "Black Aegis - Logo";
-        icon = "x\VS_C\Markers\data\logos\bapmc.paa";
+        icon = "\BAPMC-Core\Addons\markers\data\logos\bapmc_ca.paa";
+        color[] = {1,1,1,1};
+        size = 48;
+        scope = 2;
+        side = 2;
+        shadow = 0;
+        markerClass = "VS_MarkerClass";
+    };
+
+    class VS_BA_Flag_Alt
+    {
+        name = "Black Aegis - Logo (Alternative)";
+        icon = "\BAPMC-Core\Addons\markers\data\logos\bapmc_alt_ca.paa";
+        color[] = {1,1,1,1};
+        size = 48;
+        scope = 2;
+        side = 2;
+        shadow = 0;
+        markerClass = "VS_MarkerClass";
+    };
+
+    class VS_BA_Mkr_Dragon
+    {
+        name = "Black Aegis - Dragon Team";
+        icon = "\BAPMC-Core\Addons\markers\data\logos\bapmc_dragon_ca.paa";
+        color[] = {1,1,1,1};
+        size = 48;
+        scope = 2;
+        side = 2;
+        shadow = 0;
+        markerClass = "VS_MarkerClass";
+    };
+
+    class VS_BA_Mkr_Havoc
+    {
+        name = "Black Aegis - Havoc Team";
+        icon = "\BAPMC-Core\Addons\markers\data\logos\bapmc_havoc_ca.paa";
         color[] = {1,1,1,1};
         size = 48;
         scope = 2;
